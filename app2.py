ceecadba3c1c2e60b4f9b003486755ea3fb66044
Anika <bbvc331@gmail.com>
2021-07-12 00:27:02 +0800

    master

--- app2.py
+++ app2.py
@@ -6,12 +6,15 @@ from flask import redirect #載入redirect函式
 from flask import render_template
 
 #建立application 物件,可以設定靜態檔案的路徑
-app = Flask(
+app2 = Flask(
     __name__,
     static_folder="public",#靜態檔案的資料夾名稱
     static_url_path="/" #靜態檔案對應的網址路徑
     )
 
+@app2.route("/")
+def index():
+    return render_template("index.html")
 
 #啟動網站伺服器,可透過port參數指定埠號
-app.run(port=5000)
+app2.run(port=2000)
